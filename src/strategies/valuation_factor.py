ab01d1b8a107f21eeed7b375c1217be03be75fa7
J56164 <jjclub12099@gmail.com>
2026-03-14 09:25:44 +0700

    feat: valuation factor strategy (buy/sell signals)

--- src/strategies/valuation_factor.py
+++ src/strategies/valuation_factor.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import ezyquant as ez
 
+from src.utils import z_score
+
 
 def get_valuation_metrics(
     ssc: ez.SETSignalCreator,
@@ -9,17 +11,17 @@ def get_valuation_metrics(
     pb = ssc.get_data(field="pb", timeframe="daily")
     pe = ssc.get_data(field="pe", timeframe="daily")
 
-    # TODO: EV = Market Cap + Debt - Cash
+    # EV = Market Cap + Debt - Cash
     mkt_cap = ssc.get_data(field="mkt_cap", timeframe="daily")
     int_bearing_debt = ssc.get_data(field="int_bearing_debt", timeframe="yearly")
     cash = ssc.get_data(field="cash", timeframe="yearly")
     ev = mkt_cap + int_bearing_debt - cash
 
     ebitda = ssc.get_data(field="ebitda", timeframe="ytd")
-    # TODO: EV/EBITDA
+    # EV/EBITDA
     ev_div_ebitda = ev / ebitda
 
-    # TODO: FCF Yield (free_cash_flow = net_operating - net_investing, FCF Yield = free_cash_flow / market cap)
+    # FCF Yield (free_cash_flow = net_operating - net_investing, FCF Yield = free_cash_flow / market cap)
     net_operating = ssc.get_data(field="net_operating", timeframe="yearly")
     net_investing = ssc.get_data(field="net_investing", timeframe="yearly")
     free_cash_flow = net_operating - net_investing
@@ -28,27 +30,33 @@ def get_valuation_metrics(
     return pb, pe, ev_div_ebitda, fcf_yield
 
 
-def _get_buy_signal(
-    ssc: ez.SETSignalCreator,
-    close_df: pd.DataFrame,
-    high_df: pd.DataFrame,
-    low_df: pd.DataFrame,
+def get_score(
+    pb: pd.DataFrame,
+    pe: pd.DataFrame,
+    ev_div_ebitda: pd.DataFrame,
+    fcf_yield: pd.DataFrame,
 ):
-    pass  # TODO
+    return -z_score(pb) + -z_score(pe) + -z_score(ev_div_ebitda) + z_score(fcf_yield)
+
+
+def _get_buy_signal(
+    score: pd.DataFrame,
+):
+    # Long top 20%
+    pct_rank = score.rank(axis=1, pct=True)
+    buy_signal = pd.DataFrame(data=False, index=score.index, columns=score.columns)
+    buy_signal[pct_rank >= 0.8] = True
+    return buy_signal
 
 
 def _get_sell_signal(
-    ssc: ez.SETSignalCreator,
-    close_df: pd.DataFrame,
-    high_df: pd.DataFrame,
-    low_df: pd.DataFrame,
+    score: pd.DataFrame,
 ):
-    ema_20_df = ssc.ta.ema(close_df, 20)
-
-    # Sell when the current stock price is lower than 20-day moving average
-    sell_signal_df = close_df < ema_20_df
-
-    return sell_signal_df
+    # Short bottom 20%
+    pct_rank = score.rank(axis=1, pct=True)
+    sell_signal = pd.DataFrame(data=False, index=score.index, columns=score.columns)
+    sell_signal[pct_rank <= 0.2] = True
+    return sell_signal
 
 
 def _filter_signal(ssc: ez.SETSignalCreator, signal_df: pd.DataFrame):
@@ -76,17 +84,18 @@ def _rank_signal(
 
 # TODO: Rename function
 def _handle_sign_signal(ssc: ez.SETSignalCreator, signal_df: pd.DataFrame):
-    lookahead_signal = ssc.screen_universe(signal_df, mask_value=-1).shift(
-        -2
-    )  # TODO: Remove magic number
+    pass  # TODO
+    # lookahead_signal = ssc.screen_universe(signal_df, mask_value=-1).shift(
+    #     -2
+    # )  # TODO: Remove magic number
 
-    signal_df = pd.DataFrame(
-        np.where((lookahead_signal == -1), -1, signal_df),
-        columns=signal_df.columns,
-        index=signal_df.index,
-    )
+    # signal_df = pd.DataFrame(
+    #     np.where((lookahead_signal == -1), -1, signal_df),
+    #     columns=signal_df.columns,
+    #     index=signal_df.index,
+    # )
 
-    return signal_df
+    # return signal_df
 
 
 def valuation_strategy(
--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,8 @@ import pandas as pd
 
 def get_moving_average(data: pd.DataFrame, window: int):
     return data.rolling(window).mean()
+
+
+def z_score(df):
+    # Z-score across columns (stocks) for each date
+    return (df - df.mean()) / df.std()
